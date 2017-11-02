b201d5f55976a57100f4cd48b24d3df4d4a3cf26
Gerard McParland <gerard.mcparland@live.com>
2017-11-02 20:41:31 +0000

    Add project files.

---
+++ SQLDB/Class1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public class SQLDB
+{
+    private string connectionString;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="connectionString"></param>
+    public SQLDB(string connectionString)
+    {
+        this.connectionString = connectionString;
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="spName"></param>
+    public void ExecuteSP<T>(string spName)
+    {
+        using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+        {
+            using (SqlCommand sqlCommand = new SqlCommand(spName, sqlConnection))
+            {
+                sqlCommand.CommandType = CommandType.StoredProcedure;
+                sqlConnection.Open();
+                sqlCommand.ExecuteNonQuery();
+            }
+        }
+    }
+
+   /// <summary>
+   /// 
+   /// </summary>
+   /// <typeparam name="T"></typeparam>
+   /// <param name="spName"></param>
+   /// <param name="type"></param>
+    public void ExecuteSP<T>(string spName, T type)
+    {
+        using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+        {
+            using (SqlCommand sqlCommand = new SqlCommand(spName, sqlConnection))
+            {
+                sqlCommand.CommandType = CommandType.StoredProcedure;
+                sqlConnection.Open();
+                SqlCommandBuilder.DeriveParameters(sqlCommand);
+                
+                foreach (SqlParameter param in sqlCommand.Parameters)
+                {
+                    if(param.Direction == ParameterDirection.Input || param.Direction == ParameterDirection.InputOutput)
+                    {
+                        object val = type.GetType().GetProperty(param.ParameterName).GetValue(type, null);
+
+                        if (val != null)
+                        {
+                            param.Value = val;
+                        }                           
+                    }      
+                }
+                sqlCommand.ExecuteNonQuery();
+            }
+        }
+    }
+}
+
