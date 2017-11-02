0454df21578a245e6e04f5e77af6b70c701875ae
Gerard McParland <gerard.mcparland@live.com>
2017-11-02 21:34:17 +0000

    ok

--- SQLDB/Class1.cs
+++ SQLDB/Class1.cs
@@ -1,74 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data;
-using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-
-public class SQLDB
+class Class1
 {
-    private string connectionString;
-
-    /// <summary>
-    /// 
-    /// </summary>
-    /// <param name="connectionString"></param>
-    public SQLDB(string connectionString)
+    static void Main()
     {
-        this.connectionString = connectionString;
-    }
+        SQLDB db = new SQLDB("Data Source=DESKTOP-P25B241;Initial Catalog=db1;Integrated Security=True");
 
-    /// <summary>
-    /// 
-    /// </summary>
-    /// <typeparam name="T"></typeparam>
-    /// <param name="spName"></param>
-    public void ExecuteSP<T>(string spName)
-    {
-        using (SqlConnection sqlConnection = new SqlConnection(connectionString))
-        {
-            using (SqlCommand sqlCommand = new SqlCommand(spName, sqlConnection))
-            {
-                sqlCommand.CommandType = CommandType.StoredProcedure;
-                sqlConnection.Open();
-                sqlCommand.ExecuteNonQuery();
-            }
-        }
-    }
 
-   /// <summary>
-   /// 
-   /// </summary>
-   /// <typeparam name="T"></typeparam>
-   /// <param name="spName"></param>
-   /// <param name="type"></param>
-    public void ExecuteSP<T>(string spName, T type)
-    {
-        using (SqlConnection sqlConnection = new SqlConnection(connectionString))
-        {
-            using (SqlCommand sqlCommand = new SqlCommand(spName, sqlConnection))
-            {
-                sqlCommand.CommandType = CommandType.StoredProcedure;
-                sqlConnection.Open();
-                SqlCommandBuilder.DeriveParameters(sqlCommand);
-                
-                foreach (SqlParameter param in sqlCommand.Parameters)
-                {
-                    if(param.Direction == ParameterDirection.Input || param.Direction == ParameterDirection.InputOutput)
-                    {
-                        object val = type.GetType().GetProperty(param.ParameterName).GetValue(type, null);
+        Customer c = new Customer();
+        c.Name = "FREDO222aaa";
+        //c.Telephone = "111111222";
+        c.Id = 22112;
+        c.Email = "hhhEmai22l";
+            
+            db.ExecuteSP("InsertCustomer", c);
 
-                        if (val != null)
-                        {
-                            param.Value = val;
-                        }                           
-                    }      
-                }
-                sqlCommand.ExecuteNonQuery();
-            }
-        }
     }
+    
+
+
+   
+}
+public class Customer
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; }
+
+    public string Email { get; set; }
+
+    public string Telephone { get; set; }
 }
 
---
+++ SQLDB/SQLDB.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public class SQLDB
+{
+    private string connectionString;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="connectionString"></param>
+    public SQLDB(string connectionString)
+    {
+        this.connectionString = connectionString;
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="spName"></param>
+    public void ExecuteSP(string spName)
+    {
+        using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+        {
+            using (SqlCommand sqlCommand = new SqlCommand(spName, sqlConnection))
+            {
+                sqlCommand.CommandType = CommandType.StoredProcedure;
+                sqlConnection.Open();
+                sqlCommand.ExecuteNonQuery();
+            }
+        }
+    }
+
+   /// <summary>
+   /// 
+   /// </summary>
+   /// <typeparam name="T"></typeparam>
+   /// <param name="spName"></param>
+   /// <param name="type"></param>
+    public void ExecuteSP<T>(string spName, T type)
+    {
+        using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+        {
+            using (SqlCommand sqlCommand = new SqlCommand(spName, sqlConnection))
+            {
+                sqlCommand.CommandType = CommandType.StoredProcedure;
+                sqlConnection.Open();
+                SqlCommandBuilder.DeriveParameters(sqlCommand);
+                
+                foreach (SqlParameter param in sqlCommand.Parameters)
+                {
+                    if(param.Direction == ParameterDirection.Input || param.Direction == ParameterDirection.InputOutput)
+                    {
+                        object val = type.GetType().GetProperty(param.ParameterName.Replace("@","")).GetValue(type, null);
+
+                        if (val != null)
+                        {
+                            param.Value = val;
+                        }                           
+                    }      
+                }
+                sqlCommand.ExecuteNonQuery();
+            }
+        }
+    }
+    public void ExecuteSP<U>(string spName, out U t)
+    {
+        using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+        {
+            using (SqlCommand sqlCommand = new SqlCommand(spName, sqlConnection))
+            {
+                sqlCommand.CommandType = CommandType.StoredProcedure;
+                sqlConnection.Open();
+
+                foreach (SqlParameter param in sqlCommand.Parameters)
+                {
+                    if (param.Direction == ParameterDirection.Input || param.Direction == ParameterDirection.InputOutput)
+                    {
+                        object val = type.GetType().GetProperty(param.ParameterName.Replace("@", "")).GetValue(type, null);
+
+                        if (val != null)
+                        {
+                            param.Value = val;
+                        }
+                    }
+                }
+                sqlCommand.ExecuteNonQuery();
+                return sqlCommand.Parameters[1].Value 
+            }
+        }
+    }
+}
+
